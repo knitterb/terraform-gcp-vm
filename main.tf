8448ba26bf207b3c6c7be01fded0647356fc7034
Brandon Knitter <knitterb@blandsite.org>
2018-10-15 11:14:09 -0700

    new instnace

--- main.tf
+++ main.tf
@@ -30,3 +30,23 @@ resource "google_compute_instance" "default" {
     }
   }
 }
+
+resource "google_compute_instance" "test3" {
+  name         = "test3"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
