426f6881b6c26a5ebce8c11a0862aee0c9c88771
Brandon Knitter <knitterb@blandsite.org>
2018-10-15 11:02:10 -0700

    added a second VM for testing

--- main.tf
+++ main.tf
@@ -29,5 +29,24 @@ resource "google_compute_instance" "default" {
       // Ephemeral IP
     }
   }
+}
 
+resource "google_compute_instance" "default" {
+  name         = "test2"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
 }
